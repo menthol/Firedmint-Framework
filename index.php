d593ae0c62124810c105736f92896f4429974edd
menthol <commit@menthol-foundation.com>
2010-01-30 21:55:34 +0000

    don't resend headers

--- index.php
+++ index.php
@@ -5,6 +5,8 @@
 
 if (!defined(FM_SECURITY))
 {
-	header('HTTP/1.1 500 Internal Server Error');
+	if (!headers_sent())
+		header('HTTP/1.1 500 Internal Server Error');
+		
 	print '<html><head><title>500 Application Error</title></head><body><h1>Application Error</h1><p>The Firedmint application could not be launched.</p></body></html>';
 }
